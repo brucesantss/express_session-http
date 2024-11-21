755a6d214de5c9c991c087bd3b983ba6f24bb516
bruce <euryanspike.pro@gmaiul.com>
2024-11-21 15:46:22 -0300

    rota para logout

--- src/server.ts
+++ src/server.ts
@@ -12,13 +12,13 @@ app.post('/login', (req: Request, res: Response) => {
   const { username } = req.body;
 
   if (!username) {
-    res.status(400).json({ message: "Username é obrigatório" });
+    res.status(400).json({ message: "username é obrigatório." });
   }
 
   req.session.user = { username };
 
   console.log(req.sessionID);
-  res.status(200).json({ message: 'deu certo.' });
+  res.status(200).json({ message: `bem-vindo novamente, ${username}.` });
 })
 
 
@@ -31,6 +31,24 @@ app.post('/dashboard', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Dashboard de compras.' });
 });
 
+app.post('/logout', (req: Request, res: Response) => {
+
+  if(!req.session.user){
+    res.status(400).json({ message: 'você já está desconectado. fazer login?' })
+  }
+
+  const username = req.session.user.username;
+
+  req.session.destroy(err => {
+    if(err){
+      console.log('erro ao sair da conta.', err);
+      res.status(500).json({ message: 'erro ao sair da conta, tente novamente.' })
+    }
+  });
+
+  res.clearCookie('connect.sid');
+  res.status(200).json({ message: `${username} saiu da conta.`});
+})
 
 app.listen(port, () => {
   console.log(`servidor: online`);
